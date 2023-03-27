7e74e8cfba9b095774a3b52607b420f0a45843cc
sabbirjiinat <sabbirshnt@gmail.com>
2023-03-27 15:03:01 +0600

    cart section added

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './component/Header/Header'
+import Shop from './component/Shop/Shop'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-  <Header></Header>
+      <Header></Header>
+      <Shop></Shop>
       
     </div>
   )
---
+++ src/component/Product/Product.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Product.css'
+
+const Product = (props) => {
+    console.log(props);
+    const { img, name, price, quantity, ratings, seller } = props.product;
+    const handleAddToCart = props.handleAddToCart;
+    return (
+        <div className='product'>
+            <img src={img} alt="" />
+            <h5 className='product-name'>{name}</h5>
+            <div className='product-info'>
+            <p>Price: ${price}</p>
+            <p>Manufacturer: {seller}</p>
+            <p>Ratings: {ratings} star</p>
+         </div>
+            <button onClick={ () => handleAddToCart(props.product)} className='add-to-cart-btn'>Add to Cart</button>
+        </div>
+    );
+};
+
+export default Product;
